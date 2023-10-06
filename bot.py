e78e0a2e9b66dfc0bde615a832e68c23977bf4af
Rishi Duggal <72986170+RishiDuggal@users.noreply.github.com>
2023-10-06 00:44:39 -0400

    py updates + nodejs ver

--- bot.py
+++ bot.py
@@ -1,11 +1,34 @@
-import slack_sdk
+import discord
 import os
+
 from pathlib import Path
 from dotenv import load_dotenv
+from discord.ext import commands
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-client = slack_sdk.WebClient(token=os.environ['SLACK_TOKEN'])
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client.chat_postMessage(channel='#api-test', text="Hello World")
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='#', intents=intents)
+
+tree = discord.app_commands.CommandTree(bot)
+
+@bot.event
+async def on_ready():
+    await tree.sync(guild=discord.utils.get(bot.guilds, name=GUILD))
+    # print "ready" in the console when the bot is ready to work
+    print("ready")
+
+@bot.command(name='helloworld')
+async def helloworld(ctx):
+    await ctx.respond("hello world!")
+
+@tree.command(name="name", description="description")
+async def slash_command(interaction: discord.Interaction):    
+    await interaction.response.send_message("command")
+
+bot.run(TOKEN)
