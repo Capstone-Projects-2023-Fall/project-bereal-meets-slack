f20c4a85f3edb371102020cdc4c91d69f23ae304
gevdram <111989879+gevdram@users.noreply.github.com>
2023-10-21 22:40:12 -0400

    image handling

--- .github/utils/notifyMods.js
+++ .github/utils/notifyMods.js
@@ -1,6 +1,6 @@
-const {messageEmbed} = require('discord.js');
+const {MessageEmbed} = require('discord.js');
 
-async function notifyMods(guild, submissionDetails, submitter){
+async function notifyMods(guild, message){
 
     //Get role for mods
     const modRole = guild.roles.cache.find(role => role.name === 'mod all');
@@ -13,19 +13,21 @@ async function notifyMods(guild, submissionDetails, submitter){
     const moderators = guild.members.cache.filter(member => member.roles.cache.has(modRole.id));
 
     //Embedd message to include images
-    const embed = new messageEmbed()
+    const embed = new MessageEmbed()
         .setTitle('New Submission!') //title of embedded message
-        .setDescription('New Submission from ${message.author.tag}') //who submitted post
+        .setDescription(`New Submission from ${message.author.tag}`) //who submitted post
         .setColor('#000000')
     
     if (message.content){
         //include text from post with image
-        embed.addField('Post', message.content);
+        embed.addField('Post:', message.content);
     }
 
-
-
-    const content = `New submission from ${submitter.tag}: ${submissionDetails}`;
+    if(message.attachment.size > 0){
+        //if theres an attachment, include it
+        const image = message.attachment.first(); //get the first attachment
+        embed.setImage(image.url); //embed image 
+    }
 
     //Send DM to all mods
     for (const moderator of moderators.values()){
