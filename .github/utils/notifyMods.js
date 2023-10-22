8cf502781f092ab4a034abe7905a73f8955704df
gevdram <111989879+gevdram@users.noreply.github.com>
2023-10-21 20:42:46 -0400

    Update notifyMods.js

---
+++ .github/utils/notifyMods.js
@@ -0,0 +1,25 @@
+async function notifyMods(guild, submissionDetails){
+
+    //Get role for mods
+    const modRole = guild.roles.cache.find(role => role.name === 'mod all');
+
+    if(!modRole){
+        return console.error("Moderator role not found");
+    }
+
+    //Fetch memebrs with mod all role 
+    const moderators = guild.members.cache.filter(member => member.roles.cache.has(modRole.id));
+
+    const content = 'New submission: ${submissionDetails}';
+
+    //Send DM to all mods
+    for (const moderator of moderators.values()){
+        try{
+            await moderator.send(content);
+        } catch (error){
+            console.error('Cold not send DM to ${moderator.user.tag}.', error);
+        }
+    }
+}
+
+module.exports = notifyMods;
