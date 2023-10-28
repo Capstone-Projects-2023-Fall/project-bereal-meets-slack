57c4271ec048e88f541bbf3d9b157a1f262dd54e
NTRachel <to.rachel.n@gmail.com>
2023-10-28 15:07:02 -0400

    Update blacklist command

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder, Embed } = require('discord.js');
 
+const blacklistSet = new Set();
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('blacklist')
@@ -8,43 +10,41 @@ module.exports = {
         .addSubcommand(command => command.setName('remove').setDescription('Remove user from the blacklist').addStringOption(option => option.setName('user').setDescription('The user ID you want to remove from the blacklist').setRequired(true))),
         async execute (interaction){
             const {options} = interaction;
-            if (interaction.user.id !== '514962222674477066') return await interaction.reply({ content: 'Only **moderators** can use this command', ephermeral: true});
+            if (interaction.user.id !== '514962222674477066') return await interaction.reply({ content: 'Only **moderators** can use this command', ephemeral: true});
 
             const user = options.getString('user');
-            const data = await blacklist.findOne({User: user});
             const sub = options.getSubcommand();
             
             switch(sub) {
                 case 'add':
                 
-                if(!data){
-                    await blacklist.create({
-                        User: user,
-                    })
+                if(!blacklistSet.has(user)){
+                    blacklistSet.add(user);
 
                     const embed = new EmbedBuilder()
                     .setColor("Green")
                     .setDescription(`The user \`${user}\` has been blacklisted from this bot`)
 
                     await interaction.reply({embeds: [embed], ephemeral: true});
-                } else if (data) {
+                } else if (blacklistSet.has(user)) {
                     return await interaction.reply({ content: `The user \`${user}\` has already been blacklisted,`, ephemeral: true});
                 }
 
                 break;
                 case 'remove':
                 
-                if (!data) {
-                    return await interaction.reply({ content: `The user \`${user}\` is not on the blacklist,`, ephemeral: true});
-                } else if (data) {
-                    await blacklist.deleteMany({User: user});
+                if (blacklistSet.has(user)) {
+                    blacklistSet.delete(user);
 
                     const embed = new EmbedBuilder()
                     .setColor("Green")
                     .setDescription(`The user \`${user}\` has been removed from the blacklist`)
 
                     await interaction.reply({embeds: [embed], ephemeral: true});
+                } else {
+                    return await interaction.reply({ content: `The user \`${user}\` is not on the blacklist.`, ephemeral: true });
                 }
+                break;
             }
         }
 };
