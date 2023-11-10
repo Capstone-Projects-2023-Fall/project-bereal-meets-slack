1c02b2f25b7b2983bffe7017cd10b1030308bd3f
gevdram <111989879+gevdram@users.noreply.github.com>
2023-11-10 11:03:15 -0500

    Update activeHours.js

--- commands/activeHours.js
+++ commands/activeHours.js
@@ -1,5 +1,5 @@
 const {SlashCommandBuilder} = require ('discord.js');
-const {createConnectionPoolLocal} = require('./dbconn');
+const {createConnectionPoolLocal} = require('utils\dbconn.js');
 
 //Create connection from dbconn
 const pool = createConnectionPoolLocal();
