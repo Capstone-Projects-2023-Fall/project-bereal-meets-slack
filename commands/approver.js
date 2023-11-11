c50351ca7759ec16eff8457362da5727df4ac2a1
Vivek R Patel <111992734+vivekpatell3@users.noreply.github.com>
2023-11-11 13:26:52 -0700

    BMS-83

--- commands/approver.js
+++ commands/approver.js
@@ -3,8 +3,8 @@ const notifyMods = require('../utils/notifyMods.js')
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('action') //upload?
-		.setDescription('does the action')
+		.setName('submit') //upload?
+		.setDescription('Please submit your post:')
 		.addAttachmentOption(option => {
 			return option
 				.setName('file')	
@@ -30,24 +30,13 @@ module.exports = {
 			const proxy = attachment.proxyURL;
 			const type = attachment.contentType;
 
-			// console.log(JSON.stringify(attachment, null, 4));
-
-			// console.log(name);
-			// console.log(url);
-			// console.log(proxy);
-			// console.log(type);
-
-			// console.log(JSON.stringify(type, null, 4));
 			if (type) {
 				if (type.startsWith('image')) {
 					console.log('THIS IS AN IMAGE');
 
 					const caption = interaction.options.getString('caption');
 					const { responses, moderators } = await notifyMods(interaction.guild, caption, interaction.user, [attachment]);
-					// console.log(responses)
-					// console.log(moderators)
-
-					// console.log(moderators);
+				
 					const collectorFilter = i => moderators.has(i.user.id);
 
 					// const zip = (a, b) => a.map((k, i) => [k, Array.from(b)[i][1].user.globalName]);
@@ -107,28 +96,6 @@ module.exports = {
 								}
 							}
 						}
-
-						// for (const [response, moderator] of zip(responses, moderators)) {
-						// 	const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: null });
-						// 	if (approved) {
-						// 		await confirmation.update({ content: `${approver} Already Approved`, components: [] });
-						// 		break;
-						// 	}
-						
-						// 	if (confirmation.customId === 'approve') {
-						// 		approved = true;
-						// 		// console.log(moderator);
-						// 		approver = moderator;
-						// 		await confirmation.update({ content: 'Image Has Been Approved', components: [] });
-						// 		// then we do something
-						// 		const file = new AttachmentBuilder(url);
-						// 		await interaction.channel.send({ content: `(${interaction.user}) ${caption ?? '[no caption provided]'}`, files: [file]});
-								
-						// 	} else if (confirmation.customId === 'deny') {
-						// 		await confirmation.update({ content: 'Denied', components: [] });
-						// 		// then we do something
-						// 	}
-						// }
 					} catch (e) {
 						console.error('BUTTON ERROR');
 						console.error(e);
