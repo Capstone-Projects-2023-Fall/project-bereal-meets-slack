712bbfab468fbe4be861f33dc945af79f3db87d9
gevdram <111989879+gevdram@users.noreply.github.com>
2023-11-12 01:18:34 -0500

    Update activeHours.js

--- commands/activeHours.js
+++ commands/activeHours.js
@@ -11,7 +11,7 @@ module.exports = {
             const guildId = interaction.guild.id;
                 try{
                     const hours = await activeHoursUtils.fetchActiveHoursFromDB(guildId);
-                    await interaction.reply(`Active hoursare from ${hours.start_time} to ${hours.end_time}`);
+                    await interaction.reply(`Active hours are from ${hours.start_time} to ${hours.end_time}`);
                 } catch (error){
                     console.error('Failed to retrieve active hours:', error);
                     await interaction.reply('Failed to retrieve active hours.');
