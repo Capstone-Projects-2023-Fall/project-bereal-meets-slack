63a2fb80c8e3d654e4234f624329278c1c49f91d
Christian Smith <tuk96703@temple.edu>
2023-11-12 21:18:02 -0500

    quick changes to allow the cloud env to work with a file save (we are using memory now rather than local storage)

--- commands/saveData.js
+++ commands/saveData.js
@@ -1,34 +1,17 @@
 const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
-const fs = require('fs');
-const saveDB = require('../utils/saveDB'); // Adjust the path as needed
+const saveDB = require('../utils/saveDB');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('savedata')
         .setDescription('Saves data to a text file and uploads it to the chat'),
     async execute(interaction) {
-        // Defer the reply
         await interaction.deferReply();
-        
-        // Call your saveDB function
-        const savedData = await saveDB(interaction.client, process.env.DISCORD_SUBMISSION_CHANNEL_ID);
-
-        // Convert the data to a string format
+        const channelId = interaction.channelId;
+        const savedData = await saveDB(interaction.client, channelId);
         const dataString = JSON.stringify(savedData, null, 2);
-
-        // Define the filename
-        const filename = 'saved_data.txt';
-
-        // Write the string to a file
-        fs.writeFileSync(filename, dataString);
-
-        // Create a message attachment from the file
-        const fileAttachment = new AttachmentBuilder(filename, { name: 'saved_data.txt' });
-
-        // Edit the deferred reply with the file
+        const buffer = Buffer.from(dataString, 'utf-8');
+        const fileAttachment = new AttachmentBuilder(buffer, { name: 'saved_data.txt' }); //this is using RAM or memory rather than local storage
         await interaction.editReply({ content: 'Here is the saved data:', files: [fileAttachment] });
-
-        // Optional: Clean up the file after sending
-        fs.unlinkSync(filename);
     },
 };
