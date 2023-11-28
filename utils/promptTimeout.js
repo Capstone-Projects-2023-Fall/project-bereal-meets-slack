30e598f7ce53d8cca2e64fd6261213916332fe79
Vivek R Patel <111992734+vivekpatell3@users.noreply.github.com>
2023-11-27 20:51:42 -0700

    I have got the bot to now be able to set timers for prompts.

---
+++ utils/promptTimeout.js
@@ -0,0 +1,38 @@
+class PromptTimeout {
+    constructor() {
+        this.activePrompts = new Map();
+    }
+    //this will setup the timeout and message
+    setupPrompt(channelId, message) {
+        const promptId = `${channelId}-${Date.now()}`;
+        const timeoutDuration = 60000; // 60 seconds (1hr is 3600000)
+        const expiredContent = 'Post has expired.';
+
+        this.setPromptTimeout(promptId, timeoutDuration, message, expiredContent);
+    }
+
+    // this will set a timeout for a prompt
+    setPromptTimeout(promptId, duration, message, expiredContent) {
+        const expirationTime = Date.now() + duration;
+        this.activePrompts.set(promptId, expirationTime);
+        
+        setTimeout(async () => {
+            this.activePrompts.delete(promptId);
+            if (message && message.editable) {
+                await message.edit(expiredContent);
+            }
+            console.log("Prompt has expired")
+        }, duration);
+    }
+
+    // this will check if a prompt is still active
+    isPromptActive(promptId) {
+        if (!this.activePrompts.has(promptId)) {
+            return false;
+        }
+        const expirationTime = this.activePrompts.get(promptId);
+        return Date.now() < expirationTime;
+    }
+}
+
+module.exports = new PromptTimeout();
