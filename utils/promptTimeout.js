8ef0c46c224099a224c9abbd22b1dbb1103c81a7
Vivek R Patel <111992734+vivekpatell3@users.noreply.github.com>
2023-11-27 20:53:25 -0700

    promptTimeout.js has timeout duration, that is where it can be changed for testing purposes

--- utils/promptTimeout.js
+++ utils/promptTimeout.js
@@ -5,7 +5,7 @@ class PromptTimeout {
     //this will setup the timeout and message
     setupPrompt(channelId, message) {
         const promptId = `${channelId}-${Date.now()}`;
-        const timeoutDuration = 3600000; // 60 seconds (1hr is 3600000)
+        const timeoutDuration = 3600000; // 60 seconds (1hr is 3600000 & 1min is 60000)
         const expiredContent = 'Post has expired.';
 
         this.setPromptTimeout(promptId, timeoutDuration, message, expiredContent);
