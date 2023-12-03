44c64b4a69133e0c1a702d6e0f50012a03e402d4
SSunnydev <tuj11423@temple.edu>
2023-12-03 04:24:31 -0500

    prompt testing
    needs testing

--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+const getPrompts = require('../utils/promptUtils.js');
+
 var assert = require('assert');
 describe('Array', function () {
   describe('#indexOf()', function () {
@@ -41,5 +43,85 @@ describe('String Tests', () => {
     });
   });
   
+});
+
+
+//addPrompt() test
+describe('addPrompt()', () => {
+  it('a prompt was added', async () => {
+    tPrompt = "Let's see what everyone is cooking today!: "
+    const promptToAdd = tPrompt; 
+    const addResult = await addPrompt(promptToAdd);
+    assert.strictEqual(addResult, `Prompt "${promptToAdd}" has been added to the list.`);
+
+    // check if  prompt  included when retrieving prompts
+    const promptsAfterAdd = await getPrompts();
+    const addedPromptExists = promptsAfterAdd.some(prompt => prompt.prompt_text === promptToAdd);
+
+    assert.ok(addedPromptExists);
+  });
+});
+
+
+//deletePrompt() test
+describe('delete Prompt()', () => {
+  it('completely deletes prompts', async () => {
+
+    const initialPrompts = await getPrompts(); 
+    const tPrompt = initialPrompts[0];
+    await deletePrompt(tPrompt); 
+    const promptsAfterDelete = await getPrompts();
+
+    //check if the array is smaller 
+    assert.strictEqual(promptsAfterDelete.length, initialPrompts.length - 1); 
+
+    assert.ok(!promptsAfterDelete.includes(tPrompt));
+  });
+});
+
+
+
+// getPrompt() test
+describe('getPrompt()', () => {
+  it('gets zero prompts', async () => {
+    const result = await getPrompts();
+    assert.equal(result.length, 0);
+  });
+});
+
+describe('getPrompt()', () => {
+  it('gets one prompt', async () => {
+    const result = await getPrompts();
+    assert.equal(result.length, 1);
+  });
+});
+
+describe('getPrompt()', () => {
+  it('gets multiple prompts', async () => {
+    const result = await getPrompts();
+    assert.ok(result.length > 1);
+  });
+});
+
+
+//listPrompts()
+describe('listPrompts()', () => {
+  it('returns a formatted list of prompts', async () => {
+    //assume prompts in dbase
+    const result = await listPrompts();
+    assert.ok(result.startsWith('Current Prompts:'));
+  });
+});
+
+//getRandomPrompt()
+describe('getRandomPrompt()', () => {
+  it('returns a random prompt', async () => {
+    //assume prompts in dbase
+    const randomPrompt = await getRandomPrompt();
+
+    // Check if the result is not null, indicating that a prompt was retrieved
+    assert.ok(randomPrompt !== null);
+
+  });
 });
 
