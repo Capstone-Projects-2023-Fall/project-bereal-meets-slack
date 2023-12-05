e5d9e7cbc7b463540d6fab01e54854883b6e18e2
Vivek Patel <tui98034@temple.edu>
2023-12-05 15:26:29 -0700

    fixed activeHours, to be more user friendly, working towards ensuring bot is guild agnostic

--- commands/activeHours.js
+++ commands/activeHours.js
@@ -2,7 +2,9 @@ const {SlashCommandBuilder} = require ('discord.js');
 const activeHoursUtils = require('../utils/activeHoursUtils');
 const activeEvents = require('../utils/activeEvents');
 
-
+function isValidTime(time) {
+    return /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/.test(time);
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -24,36 +26,38 @@ module.exports = {
           ),
 
       
-        async execute(interaction){
+          async execute(interaction) {
             const subcommand = interaction.options.getSubcommand();
             const guildId = interaction.guild.id;
-            switch(subcommand){
-                case 'list':
-                    await interaction.deferReply();
-                    try{
-                        const hours = await activeHoursUtils.fetchActiveHoursFromDB(guildId);
-                        await interaction.followUp(`Active hours are from ${hours.start_time} to ${hours.end_time}`);
-                    } catch (error){
-                        console.error('Failed to retrieve active hours:', error);
-                        await interaction.followUp('Failed to retrieve active hours.');
-                    }
-                    break;
-                
-                case 'set':
-                    await interaction.deferReply(); 
-                        const startTime = interaction.options.getString('start-time');
-                        const endTime = interaction.options.getString('end-time');
-                            try{
-                                await activeHoursUtils.storeOperatingHours(guildId, startTime, endTime);
-                                // will emit signal to reschedule the posts (after active hours have been changed)
-                                activeEvents.emit('activeHoursUpdated', { guildId: interaction.guild.id, start_time: startTime, end_time: endTime });
-                                console.log("Post scheduling during new active hours");
-                                await interaction.followUp(`Active hours set from ${startTime} to ${endTime}`);
-                            } catch (error){
-                                console.error('Failed to set active hours:', error);
-                                await interaction.followUp('There was an error setting active hours.');
-                            }
-                    break;
+    
+            if (subcommand === 'list') {
+                await interaction.deferReply();
+                try {
+                    const hours = await activeHoursUtils.fetchActiveHoursFromDB(guildId);
+                    await interaction.followUp(`Active hours are from ${hours.start_time} to ${hours.end_time}`);
+                } catch (error) {
+                    console.error('Failed to retrieve active hours:', error);
+                    await interaction.followUp('Failed to retrieve active hours.');
+                }
+            } else if (subcommand === 'set') {
+                await interaction.deferReply();
+                const startTime = interaction.options.getString('start-time');
+                const endTime = interaction.options.getString('end-time');
+    
+                if (!isValidTime(startTime) || !isValidTime(endTime)) {
+                    await interaction.followUp('Invalid time format. Please use HH:MM format.');
+                    return;
+                }
+    
+                try {
+                    await activeHoursUtils.storeOperatingHours(guildId, startTime, endTime);
+                    activeEvents.emit('activeHoursUpdated', { guildId, start_time: startTime, end_time: endTime });
+                    console.log("Post scheduling during new active hours");
+                    await interaction.followUp(`Active hours set from ${startTime} to ${endTime}`);
+                } catch (error) {
+                    console.error('Failed to set active hours:', error);
+                    await interaction.followUp('There was an error setting active hours.');
+                }
             }
         }
     };
