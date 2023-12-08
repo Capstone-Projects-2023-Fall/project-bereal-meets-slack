261ecbd74deaa0af5901e53f37bf12f54eabf851
Tara Feeley <tup31461@temple.edu>
2023-12-08 16:39:47 -0500

    Had to remove destructuring for the test to correctly check if the function was called; really stupid that that was causing problems

--- test/promptCommandTest.js
+++ test/promptCommandTest.js
@@ -1,15 +1,16 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
+const promptCommand = require('../commands/prompt-commands');
 const promptUtils = require('../utils/promptUtils');
 
 describe(('prompt command'), () => {
     let interaction;
 
     beforeEach(() => {
-        sinon.stub(promptUtils, 'getPrompts');
+        // sinon.stub(promptUtils, 'listPrompts');
         sinon.stub(promptUtils, 'addPrompt');
         sinon.stub(promptUtils, 'deletePrompt');
-        sinon.stub(promptUtils, 'deletePrompt');
+        sinon.stub(promptUtils, 'searchPrompts');
 
         interaction = {
             options: {
@@ -18,12 +19,23 @@ describe(('prompt command'), () => {
             },
             guild: { id: process.env.DISCORD_GUILD_ID },
             deferReply: sinon.fake(),
-            followUp: simon.fake(),
+            followUp: sinon.fake(),
         };
     });
 
     afterEach(() => {
-        sinon.restor();
+        sinon.restore();
     });
 
+    it('should list the prompts', async () => {
+        const listPromptsStub = sinon.stub(promptUtils, 'listPrompts');
+
+        interaction.options.getSubcommand.returns('list');
+
+        await promptCommand.execute(interaction);
+
+        expect(interaction.deferReply.called).to.be.true;
+        expect(promptUtils.listPrompts.called).to.be.true;
+        listPromptsStub.restore(); 
+    });
 });
