59bdabcfa73c345faab36efc76354938772443ed
gevdram <111989879+gevdram@users.noreply.github.com>
2023-12-09 02:07:13 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -192,10 +192,9 @@ async function schedulePost(activeHoursData){
 
 async function postPrompt(callingUser) {
     const promptData = await promptUtils.getRandomPrompt();
-    console.log("Fetched Prompt Data:", promptData);
 
     if (!promptData) {
-        console.error("No prompt found for the guild.");
+        console.error("No prompt found.");
         return;
     }
 
