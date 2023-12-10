a5213593d82b7f6366d7a44a3c204d3fc5316164
Rishi Duggal <72986170+RishiDuggal@users.noreply.github.com>
2023-12-09 20:52:04 -0500

    hard coding guild id

--- utils/commandregistrar.js
+++ utils/commandregistrar.js
@@ -42,7 +42,7 @@ function registercommands(){
   
       // The put method is used to fully refresh all commands in the guild with the current set
       const data = await rest.put(
-        Routes.applicationCommands(CLIENT_ID),
+        Routes.applicationGuildCommands(CLIENT_ID, process.env.DISCORD_GUILD_ID),
         { body: commands },
       );
     
