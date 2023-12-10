70b0079373965af490a692b91c23ef3e0a16b550
gevdram <111989879+gevdram@users.noreply.github.com>
2023-12-10 11:34:46 -0500

    help

--- bot.js
+++ bot.js
@@ -14,6 +14,8 @@ const { prompt } = require('./utils/prompt.js');
 const PromptTimeout = require('./utils/promptTimeout');
 const activeEvents = require('./utils/activeEvents')
 const setDefaultChannel = require('./commands/setDefaultChannel.js');
+const helpUtils = require('../utils/helpUtils.js');
+
 
 
 //for cloud run, serverless application needs a server to listen.
@@ -303,34 +305,9 @@ client.on('messageCreate', async msg => {
 client.on('guildCreate', async guild => {
     await setDefaultChannel(guild.systemChannel.id, guild.id);
 
-    const welcomeMessage =`
-    Hello! It is the bot, beep bop boop.
-    \n Please make sure to use /setsubmissionchannel to change the default channel of the bot.
-    \n Use /prompts add to add new prompts, /prompt delete to delete a prompt, and /prompt list to list all of your prompts. Be sure to assign them to the corresponding channel.
-    \n Use /activehours set to setup the random prompting feature.
-    \n Use /blacklist to manage users who are not able to be prompted. Users have 3 strikes (denials) before they are automatically blacklisted.
-    \n Use /exportcsv to retrieve a copy of reaction data from the server.
-    \n Use /graphdata to show a graph of the reaction data on screen.
-    \n Users can use /notifications to enable or disable notifications of new posts.
-    \n Users can use /toggle_private to receive their message prompt as a DM. 
-    \n Users should use /submit when prompted to upload their images with a caption in the corresponding channel.
-    \n
-    \n Use /help to review this message again. `;
-
+    const welcomeMessage = helpUtils.getHelpMessage();
     guild.systemChannel.send(welcomeMessage);
-    // guild.systemChannel.send(`
-    // Hello! It is the bot.
-    // \n Please make sure to use /setsubmissionchannel to change the default channel of the bot.
-    // \n Use /prompts add to add new prompts, /prompt delete to delete a prompt, and /prompt list to list all of your prompts. Be sure to assign them to the corresponding channel.
-    // \n Use /activehours set to setup the random prompting feature.
-    // \n Use /blacklist to manage users who are not able to be prompted. Users have 3 strikes (denials) before they are automatically blacklisted.
-    // \n Use /exportcsv to retrieve a copy of reaction data from the server.
-    // \n Use /graphdata to show a graph of the reaction data on screen.
-    // \n Users can use /notifications to enable or disable notifications of new posts.
-    // \n Users can use /toggle_private to receive their message prompt as a DM. 
-    // \n Users should use /submit when prompted to upload their images with a caption in the corresponding channel.
-    // \n
-    // \n Use /help to review this message again. `) //fix me
+    
 });
 
 // Make sure this line is the last line
