c3c104b2427e519fd620fb13da412581462c2be2
gevdram <111989879+gevdram@users.noreply.github.com>
2023-12-10 16:10:48 -0500

    update

--- commands/graph.js
+++ commands/graph.js
@@ -8,6 +8,9 @@ module.exports = {
         .setDescription('Displays a graph based on the data'),
 
     async execute(interaction) {
+        const modRole = interaction.guild.roles.cache.find(role => role.name === 'bot mod');
+            
+        if (!(interaction.member.roles.cache.has(modRole.id))) return await interaction.reply({ content: 'Only **moderators** can use this command', ephemeral: true});
 
         await interaction.deferReply();
         try {
